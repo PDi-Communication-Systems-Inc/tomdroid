3812924833f3a51141d11b2ae5342005d322c39e
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-05-24 11:55:30 -0400

    Changes to compile under AOSP

---
+++ Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under)
+LOCAL_PACKAGE_NAME := org.tomdroid
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+
+# List of static libraries to include in the package
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := signpost-commons-http:libs/signpost-commonshttp4-1.2.1.2.jar \
+                                        signpost-core:libs/signpost-core-1.2.1.2.jar \
+					diffutils:libs/diffutils-1.2.1.jar
+
+include $(BUILD_MULTI_PREBUILT)
