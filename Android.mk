604c31d670b6c29a915188bc330e4e56a9506efe
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-05-25 09:16:09 -0400

    Fix Android.mk so that it actually builds the tomdroid app during AOSP builds, this one seems to work

--- Android.mk
+++ Android.mk
@@ -8,8 +8,17 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
 
 # List of static libraries to include in the package
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := signpost-commons-http:libs/signpost-commonshttp4-1.2.1.2.jar \
-                                        signpost-core:libs/signpost-core-1.2.1.2.jar \
-					diffutils:libs/diffutils-1.2.1.jar
+LOCAL_JAVA_LIBRARIES := signpost-commons-http \
+                        signpost-core \
+		        diffutils \
+			android.test.runner
+
+include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+
+LOCAL_STATIC_JAVA_LIBRARIES := signpost-commons-http:libs/signpost-commonshttp4-1.2.1.2.jar \
+                               signpost-core:libs/signpost-core-1.2.1.2.jar \
+                               diffutils:libs/diffutils-1.2.1.jar
 
 include $(BUILD_MULTI_PREBUILT)
